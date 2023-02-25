85061d4fad2f04d83e0ba37e5f9c24edb55b56fa
Kerrion Siokwu <ladykerrion@sky.com>
2023-02-25 02:46:31 +0000

    Added initial questions for the user

--- index.js
+++ index.js
@@ -13,3 +13,56 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+// Array of questions for user
+const questions = [
+  {
+    message: "What is the team manager's name?",
+    type: "input",
+    name: "manager-name",
+  },
+  {
+    message: "What is the team manager's Employee ID?",
+    type: "input",
+    name: "manager-id",
+  },
+  {
+    message: "What is the team manager's Email?",
+    type: "input",
+    name: "manager-email",
+    validate: function (email) {
+  
+      valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+
+      if (valid) {
+        console.log("Great job");
+          return true;
+      } else {
+          console.log(".  Please enter a valid email")
+          return false;
+      }
+    },
+  },
+  {
+    message: "What is the team manager's office number?",
+    type: "input",
+    name: "manager-office-number",
+  },
+  {
+    message: "Please select a team member to add",
+    type: "list",
+    name: "employee-option",
+    choices: ["Add an Engineer", "Add an Intern", "Finish building the team",]
+  },
+
+];
+
+// Function to initialize program
+function init() {
+  inquirer.prompt(questions)
+    .then((response) => {
+
+    });
+}
+
+// Function call to initialize program
+init();
